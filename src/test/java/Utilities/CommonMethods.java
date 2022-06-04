99323a8e0c0bfb14f29227feb307c91927d8ac6c
Tola Chea <Cheatola1768@gmail.com>
2022-06-04 08:59:02 -0400

    Update Utilities class

--- src/test/java/Utilities/CommonMethods.java
+++ src/test/java/Utilities/CommonMethods.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.UnexpectedTagNameException;
@@ -22,44 +23,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class CommonMethods {
-	/**
-	 * Method that clears and sends keys
+
+	/*
+	 *Method that clear and sends key
+	 * By: 
 	 * 
-	 * @param element
-	 * @param text
-	 */
+	 * 
+	 * */
 	public static void sendText(WebElement element, String text) {
 		element.clear();
 		element.sendKeys(text);
 	}
-
-	/**
-	 * Method checks if radio/checkbox is enabled and clicks it
+	
+	/*
+	 * Method checks if radio / check box is enabled and clicked
 	 * 
-	 * @param radioOrcheckbox
-	 * @param value
-	 */
-	public static void clickRadioOrCheckbox(List<WebElement> radioOrcheckbox, String value) {
-
+	 * By
+	 * */
+	public static void clickRadioOrCheckbox(List <WebElement> radioOrCheckbox, String value) {
 		String actualValue;
-
-		for (WebElement el : radioOrcheckbox) {
+		for (WebElement el:radioOrCheckbox) {
 			actualValue = el.getAttribute("value").trim();
-			if (el.isEnabled() && actualValue.equals(value)) {
+			if(el.isEnabled()&& actualValue.equals(value)) {
 				el.click();
 				break;
 			}
-		}
+		}	
 	}
-
-	/**
-	 * Method that checks if text is there and then selects it
-	 * 
-	 * @param element
-	 * @param textToSelect Author: Shafkat ALi
-	 */
+	
+	//SELECT DROPDOWN VALUE
 	public static void selectDdText(WebElement element, String textToSelect) {
-
 		try {
 			Select select = new Select(element);
 
@@ -77,18 +70,11 @@ public class CommonMethods {
 		}
 	}
 
-	/**
-	 * Method that selects value by index
-	 * 
-	 * @param element
-	 * @param index
-	 */
+	//SELECT VALUE INDEX
 	public static void selectDdIndex(WebElement element, int index) {
-
 		try {
 			Select select = new Select(element);
 			int size = select.getOptions().size();
-
 			if (size > index) {
 				select.selectByIndex(index);
 			}
@@ -96,30 +82,26 @@ public class CommonMethods {
 			e.printStackTrace();
 		}
 	}
-
+	
 	/**
 	 * Methods that accept alerts and catches exception if alert is not present
 	 */
 	public static void acceptAlert() {
-
 		try {
 			Alert alert = BaseClass.getDriver().switchTo().alert();
 			alert.accept();
-
 		} catch (NoAlertPresentException e) {
 			e.printStackTrace();
-		}
+			}
 	}
 
 	/**
 	 * Methods that dismiss alerts and catches exception if alert is not present
 	 */
 	public static void dismissAlert() {
-
 		try {
 			Alert alert = BaseClass.getDriver().switchTo().alert();
 			alert.dismiss();
-
 		} catch (NoAlertPresentException e) {
 			e.printStackTrace();
 		}
@@ -131,16 +113,13 @@ public class CommonMethods {
 	 * @return String alert text
 	 */
 	public static String getAlertText() {
-
 		String alertText = null;
-
 		try {
 			Alert alert = BaseClass.getDriver().switchTo().alert();
 			alertText = alert.getText();
-		} catch (NoAlertPresentException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		return alertText;
 	}
 
@@ -153,168 +132,157 @@ public class CommonMethods {
 		try {
 			Alert alert = BaseClass.getDriver().switchTo().alert();
 			alert.sendKeys(text);
+			alert.accept();
 		} catch (NoAlertPresentException e) {
 			e.printStackTrace();
 		}
 	}
 	
-//Dealing with Iframe
-	public static void switchToFrame(String nameOrId) {
-
-		try {
-			BaseClass.getDriver().switchTo().frame(nameOrId);
-		} catch (NoSuchFrameException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void switchToFrame(WebElement element) {
-
-		try {
-			BaseClass.getDriver().switchTo().frame(element);
-		} catch (NoSuchFrameException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void switchToFrame(int index) {
-
-		try {
-			BaseClass.getDriver().switchTo().frame(index);
-		} catch (NoSuchFrameException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Method switches focus to child window
-	 */
-	public static void switchToChildWindow() {
-		String mainWindow = BaseClass.getDriver().getWindowHandle();
-		Set<String> windows = BaseClass.getDriver().getWindowHandles();
-		for (String window : windows) {
-			if (!window.equals(mainWindow)) {
-				BaseClass.getDriver().switchTo().window(window);
-				break;
+	//Dealing with Iframe
+		public static void switchToFrame(String nameOrId) {
+			try {
+				BaseClass.getDriver().switchTo().frame(nameOrId);
+			} catch (NoSuchFrameException e) {
+				e.printStackTrace();
 			}
 		}
-	}
-
-	public static WebDriverWait getWaitObject() {
-		WebDriverWait wait = new WebDriverWait(BaseClass.getDriver(), 10);
-		return wait;
-	}
-
-	public static WebElement waitForClickability(WebElement element) {
-		return getWaitObject().until(ExpectedConditions.elementToBeClickable(element));
-	}
-
-	public static WebElement waitForVisibility(WebElement element) {
-		return getWaitObject().until(ExpectedConditions.visibilityOf(element));
-	}
-
-	public static void click(WebElement element) {
-		waitForClickability(element);
-		element.click();
-	}
-
-	public static JavascriptExecutor getJSObject() {
-		JavascriptExecutor js = (JavascriptExecutor) BaseClass.getDriver();
-		return js;
-	}
-
-	public static void jsClick(WebElement element) {
-		getJSObject().executeScript("arguments[0].click();", element);
-	}
-
-	public static void scrollToElement(WebElement element) {
-		getJSObject().executeScript("arguments[0].scrollIntoView(true);", element);
-	}
-
-	/**
-	 * Method that will scroll the page down based on the passed pixel parameters
-	 * 
-	 * @param pixel
-	 */
-	public static void scrollDown(int pixel) {
-		getJSObject().executeScript("window.scrollBy(0," + pixel + ")");
-	}
-
-	/**
-	 * Method that will scroll the page up based on the passed pixel parameters
-	 * 
-	 * @param pixel
-	 */
-	public static void scrollUp(int pixel) {
-		getJSObject().executeScript("window.scrollBy(0,-" + pixel + ")");
-	}
-
-	/**
-	 * This Method will take a screenshot
-	 * 
-	 * @param filename
-	 */
-	public static byte[] takeScreenshot(String filename) {
-		TakesScreenshot ts = (TakesScreenshot) BaseClass.getDriver();
-		byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
-
-		File file = ts.getScreenshotAs(OutputType.FILE);
-		String destinationFile = "" + filename + getTimeStemp() + ".png";
-
-		try {
-			FileUtils.copyFile(file, new File(destinationFile));
-		} catch (Exception ex) {
-			System.out.println("Cannot take screenshot!");
+		
+		public static void switchToFrame(WebElement element) {
+			try {
+				BaseClass.getDriver().switchTo().frame(element);
+			} catch (NoSuchFrameException e) {
+				e.printStackTrace();
+			}
 		}
-
-		return picBytes;
-	}
-
-	public static String getTimeStemp() {
-		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
-		return sdf.format(date.getTime());
-	}
-
-	public static void wait(int second) {
-		try {
-			Thread.sleep(second * 1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		
+		public static void switchToFrame(int index) {
+			try {
+				BaseClass.getDriver().switchTo().frame(index);
+			} catch (NoSuchFrameException e) {
+				e.printStackTrace();
+			}
 		}
-	}
-
-	/**
-	 * this method will select a date from the calendar
-	 * 
-	 * @param element
-	 * @param text
-	 */
-
-	public static void selectCalendarDate(List<WebElement> element, String text) {
-		for (WebElement pickDate : element) {
-			if (pickDate.isEnabled()) {
-				if (pickDate.getText().equals(text)) {
-					pickDate.click();
+		
+		/**
+		 * Method switches focus to child window
+		 */
+		public static void switchToChildWindow() {
+			String mainWindow = BaseClass.getDriver().getWindowHandle();
+			Set<String> windows = BaseClass.getDriver().getWindowHandles();
+			for (String window : windows) {
+				if (!window.equals(mainWindow)) {
+					BaseClass.getDriver().switchTo().window(window);
 					break;
 				}
 			}
 		}
-	}
-
-	/**
-	 * This Method will take a screenshot
-	 * 
-	 * @param filename
-	 */
-	public static void TakesScreenshot(String fileName) {
-		TakesScreenshot ts = (TakesScreenshot) BaseClass.getDriver();
-		File screen = ts.getScreenshotAs(OutputType.FILE);
-		try {
-			FileUtils.copyFile(screen, new File("screenshot/" + fileName + ".png"));
-		} catch (IOException e) {
-			System.out.println("Cannot take screenshot");
-			// e.printStackTrace();
+		
+		//WAIT METHODS
+		
+		public static WebDriverWait getWaitObject() {
+			WebDriverWait wait = new WebDriverWait(BaseClass.getDriver(),Constants.explicitWaitTime);
+			return wait;
 		}
-	}
+		
+		public static WebElement waitForClickability(WebElement element) {
+			return getWaitObject().until(ExpectedConditions.elementToBeClickable(element));
+		}
+		
+		public static WebElement waitForVisibility(WebElement element) {
+			return getWaitObject().until(ExpectedConditions.visibilityOf(element));
+		}
+		
+		//CLICK METHOD
+		public static void click(WebElement element) {
+			waitForClickability(element);
+			element.click();
+		}
+		
+		//JS EXECUTER
+		public static JavascriptExecutor getJSObject() {
+			JavascriptExecutor js = (JavascriptExecutor) BaseClass.getDriver();
+			return js;
+		}
+
+		public static void jsClick(WebElement element) {
+			getJSObject().executeScript("arguments[0].click();", element);
+		}
+
+		public static void scrollToElement(WebElement element) {
+			getJSObject().executeScript("arguments[0].scrollIntoView(true);", element);
+		}
+		
+		//SCROLL USING PIXEL
+		/**
+		 * Method that will scroll the page down based on the passed pixel parameters
+		 * 
+		 * @param pixel
+		 */
+		public static void scrollByPixel(int pixel) {
+			//scroll down positive 
+			//scroll up negative
+			getJSObject().executeScript("window.scrollBy(0," + pixel + ")");
+		}
+
+		/**
+		 * This Method will take a screenshot
+		 * 
+		 * @param filename
+		 */
+		public static String getTimeStemp() {
+			Date date = new Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+			return sdf.format(date.getTime());
+		}
+
+		public static byte[] takeScreenshot(String filename) {
+			TakesScreenshot ts = (TakesScreenshot) BaseClass.getDriver();
+			byte[] picBytes = ts.getScreenshotAs(OutputType.BYTES);
+			File file = ts.getScreenshotAs(OutputType.FILE);
+			String destinationFile = Constants.SCREENSHOT_FILEPATH+ getTimeStemp() + ".png";
+			try {
+				FileUtils.copyFile(file, new File(destinationFile));
+			} catch (Exception ex) {
+				System.out.println("Cannot take screenshot!");
+			}
+
+			return picBytes;
+		}
+		
+
+		/**
+		 * this method will select a date from the calendar
+		 * 
+		 * @param element
+		 * @param text
+		 */
+		public static void selectCalendarDate(List<WebElement> element, String text) {
+			for (WebElement pickDate : element) {
+				if (pickDate.isEnabled()) {
+					if (pickDate.getText().equals(text)) {
+						pickDate.click();
+						break;
+					}
+				}
+			}
+		}
+		
+		//HOVER
+		
+		public static void hover(WebElement element) {
+			Actions actions = new Actions(BaseClass.getDriver());
+			actions.moveToElement(element).perform();
+		}
+		
+		//DRAGANDDROP METHOD
+		public static void dragAndDrop(WebElement source, WebElement target) {
+			Actions actions = new Actions(BaseClass.getDriver());
+			actions.dragAndDrop(source, target).perform();
+		}
+		
+		//DOUBLE CLICK
+		public static void doubleClick(WebElement source) {
+			Actions actions = new Actions(BaseClass.getDriver());
+			actions.doubleClick(source).perform();
+		}
 }
--- src/test/java/Utilities/Constants.java
+++ src/test/java/Utilities/Constants.java
@@ -2,9 +2,15 @@ package Utilities;
 
 public class Constants {
 	
-public static final String config_filePath = System.getProperty("user.dir")+ "/src/test/resources/propertiesFolder/config.properties";
+	public static final String config_filePath = System.getProperty("user.dir")+ "/src/test/resources/propertiesFolder/config.properties";
+	
+	public static String excel_filePath = "./src/test/resources/excelFolder/tolaExcelSheet2.xlsx";
+	
+	public static final String SCREENSHOT_FILEPATH = "./test-output/screenshot";
 	
 	public static final int implicitWaitTime = 10;
+	
+	public static final int explicitWaitTime = 10;
 	
 	public static final int slidersCount = 3;
 	
@@ -12,6 +18,12 @@ public static final String config_filePath = System.getProperty("user.dir")+ "/s
 	
 	public static final String quantityCountHPTest7 = "5827";
 	public static final String quantityCountHPTest8 = "10";
+	
+
+	
+	
+	
+	
 	
 
 }
